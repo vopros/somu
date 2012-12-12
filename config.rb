7d7c11f93e97788216e61cff1e619c897e29681b
George Timoschenko <malfermu@malfer.mu>
2012-12-12 15:38:46 +0400

    Base commit

---
+++ config.rb
@@ -0,0 +1,13 @@
+# Init
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
+output_style = :compressed # :expanded or :nested or :compact or :compressed
+preferred_syntax = :sass
+
+# relative_assets = true
+# line_comments = false
+
