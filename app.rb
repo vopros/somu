3de36454d374538aa6c2f62da74d94ff814e0064
George Timoschenko <malfermu@malfer.mu>
2012-12-21 18:20:28 +0400

    Better routing

--- app.rb
+++ app.rb
@@ -7,22 +7,23 @@ require 'slim'
 
 configure do
   Compass.add_project_configuration './settings/compass.rb'
-  set :show_exceptions, false
+  set :show_exceptions, false #
 end
 
 error { File.read $error }
 set :public, $public
 set :port, $port
 
+# SASS/Compass & CSS
 get '/assets/*.css' do
   css = params[:splat].first
   style = "#{$styles}/#{css}.css"
   return File.read(style) if File.exists?(style)
 
-  set :views, $styles
-  sass :"#{css}", Compass.sass_engine_options
+  sass :"#{css}", Compass.sass_engine_options.merge(views: $styles)
 end
 
+# JS & CoffeeScript
 get '/assets/*.js' do
   js = params[:splat].first
   script = "#{$scripts}/#{js}.js"
@@ -33,6 +34,7 @@ get '/' do
   slim :index
 end
 
+# Slim & HTML
 get '/*/?' do
   page = params[:splat].first
   html = "#{$views}/#{page}.html"
