b56ee7eb0e32f4077a1fd0c6ef50d1fed0835182
George Timoschenko <malfermu@malfer.mu>
2012-12-21 23:30:58 +0400

    Fosterhunter privacy

--- settings/instajour.rb
+++ settings/instajour.rb
@@ -1,19 +1,8 @@
 # encoding: utf-8
 
-=begin fosterhunter
-  $instajour = Instajour.new(
-    '30610955.5d3b7bd.124b5a19c8ad42e6b5943ea520cc3149',
-    'Инстажур Фостера',
-    'Фостер Хантер',
-    ''
-  )  
-=end
-
-#=begin
 $instajour = Instajour.new(
   '206005842.5d3b7bd.5a58ba2f68e247a9b97839bf6a5eb6a0',
   'Инстажур Георгия',
   'Георгий Тимощенко',
   ''
 )
-#=end
