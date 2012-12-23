ec2818baf407431c2be3a62f4ea5391cf8c8b0e8
George Timoschenko <malfermu@malfer.mu>
2012-12-23 23:40:15 +0400

    H1 → Title

---
+++ public/main.js
@@ -0,0 +1,13 @@
+document.$scroll = function() {
+  var y = $(window).scrollTop();
+  var title = '';
+  $ ('h1').each (function() {
+    if ($(this).position().top > y + window.innerHeight) return false;
+    title = $(this).text();
+    if ($(this).position().top > y) return false;
+  });
+  if (title) document.title = title;
+};
+
+$(window).bind ('scroll', document.$scroll);
+window.onload = function () {document.$scroll();};
